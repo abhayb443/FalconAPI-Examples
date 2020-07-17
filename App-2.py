4ee70387aeca9e302e4664f1b138b430dfeb4115
abhayb443 <pandeyabhay443@gmail.com>
2020-07-17 14:52:37 +0530

    API with GET request and works with methods via postman, added support for HTTP status codes

---
+++ App-2.py
@@ -0,0 +1,39 @@
+import falcon
+import json
+from waitress import serve
+
+
+class ObjReq:
+    def on_get(self, req, resp):
+
+        resp.status = falcon.HTTP_200
+
+        data = json.loads(req.stream.read())
+
+        dic = {'Name': 'Abhay','Age': 26, 'Status': 'Employed', 'Type': 'IT'}
+
+        doc = {
+            'images': [
+                dic
+            ],
+
+            'blows': [
+                dic
+            ]
+        }
+
+        output = {}
+
+        if data['method'] == 'on_get':
+            output['name'] = doc.get('images')[0].get('Name')
+        else:
+            resp.status = falcon.HTTP_404
+            output['name'] = None
+
+        resp.body = json.dumps(output)
+
+
+api = application = falcon.API()
+api.add_route('/test', ObjReq())
+
+serve(api, host='127.0.0.1', port=5555)
